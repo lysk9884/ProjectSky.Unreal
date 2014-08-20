35ba06a4372ed0c13b8eb4a812da49e8a771c8a8
Ilseob lee <ilseob.lee@gumi.sg>
2014-08-21 00:58:34 +0800

    first commit

---
+++ ProjectSky/Intermediate/Build/Mac/ProjectSky/DebugGame/DylibCopy.sh
@@ -0,0 +1,5 @@
+cp -f "/Users/Shared/UnrealEngine/4.4/Engine/Source/ThirdParty/IntelTBB/IntelTBB-4.0/lib/Mac/libtbb.dylib" "$1.app/Contents/MacOS"
+cp -f "/Users/Shared/UnrealEngine/4.4/Engine/Source/ThirdParty/IntelTBB/IntelTBB-4.0/lib/Mac/libtbbmalloc.dylib" "$1.app/Contents/MacOS"
+cp -f "/Users/Shared/UnrealEngine/4.4/Engine/Source/ThirdParty/FreeType2/FreeType2-2.4.12/Lib/Mac/libfreetype.dylib" "$1.app/Contents/MacOS"
+cp -f "/Users/Shared/UnrealEngine/4.4/Engine/Source/ThirdParty/Ogg/libogg-1.2.2/macosx/libogg.dylib" "$1.app/Contents/MacOS"
+cp -f "/Users/Shared/UnrealEngine/4.4/Engine/Source/ThirdParty/Vorbis/libvorbis-1.3.2/macosx/libvorbis.dylib" "$1.app/Contents/MacOS"
---
+++ ProjectSky/Source/ProjectSky/Private/ProjectSky.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectSky.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectSky, "ProjectSky" );
+
+DEFINE_LOG_CATEGORY(LogProjectSky)
+ 
