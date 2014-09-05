e6cba934fa4050b4ee59bc1b2a7af1ff775dc5cf
Ilseob lee <ilseob.lee@gumi.sg>
2014-09-05 17:44:44 +0800

    - stop tracking unnecessary files

--- ProjectSky/Intermediate/Build/Mac/ProjectSkyEditor/DebugGame/ProjectSky/ProjectSky.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_PROJECTSKY_H__
-#define __AUTO_PROJECTSKY_H__
-//Last Write: 3/9/2014 3:59:40 AM +00:00
-#include "/Users/ilseob.lee/Documents/GIt/ProjectSky.Unreal/ProjectSky/Source/ProjectSky/Public/ProjectSky.h"
-#endif//__AUTO_PROJECTSKY_H__
